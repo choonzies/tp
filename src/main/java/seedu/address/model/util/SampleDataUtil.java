9756c57346081ae67ef6a1c9900aa61f6603f6ef
choonzies <pohjingchoon@gmail.com>
2024-11-07 23:48:14 +0800

    Update SampleDataUtil.java

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,11 +1,15 @@
 package seedu.address.model.util;
 
+import static seedu.address.commons.util.DateUtil.DATE_TIME_FORMATTER;
+
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.appointment.Appointment;
 import seedu.address.model.person.Diagnosis;
 import seedu.address.model.person.Id;
 import seedu.address.model.person.Medication;
@@ -22,34 +26,20 @@ public class SampleDataUtil {
     public static final Notes EMPTY_NOTES = new Notes("");
     public static Person[] getSamplePersons() {
         return new Person[] {
-            /*
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
-
-             */
             new Person(new Name("Alex Yeoh"), new Id("P12345"), new Ward("A1"),
                     new Diagnosis("A. fib (Atrial Fibrillation)"),
-                    new Medication("Metoprolol"), EMPTY_NOTES),
+                    new Medication("Metoprolol"),
+                    new Notes("Patient is allergic to Paracetemol."),
+                    new Appointment("Surgery",
+                            LocalDateTime.parse("01-01-2001-01-00", DATE_TIME_FORMATTER),
+                            LocalDateTime.parse("01-01-2001-07-00", DATE_TIME_FORMATTER))),
             new Person(new Name("Bernice Yu"), new Id("P23456"), new Ward("B1"),
                     new Diagnosis("Celiac Disease/Gluten Sensitivity"),
                     new Medication("Corticosteroids"),
-                    new Notes("patient is on a strict gluten free diet")),
+                    new Notes("patient is on a strict gluten free diet"),
+                    new Appointment("Surgery",
+                            LocalDateTime.parse("01-01-2001-01-00", DATE_TIME_FORMATTER),
+                            LocalDateTime.parse("01-01-2001-07-00", DATE_TIME_FORMATTER))),
             new Person(new Name("Charlotte Oliveiro"), new Id("P34567"), new Ward("C2"),
                     new Diagnosis("Major Depressive Disorder - Recurrent"),
                     new Medication("Fluoxetine (Prozac)"), EMPTY_NOTES),
